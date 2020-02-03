f2f5144114a79778bae20238d3a8c22f5091f86c
Timothey-V <Tim170298>
2020-02-03 11:31:12 +0100

    Final version with tests ans controller

--- src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/game_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -2,24 +2,30 @@ package com.miage.altea.game_ui.pokemonTypes.service;
 
 import com.miage.altea.game_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
-public class PokemonTypeServiceImpl {
+public class PokemonTypeServiceImpl implements PokemonTypeService{
+    RestTemplate restTemplate;
+    String pokemonServiceUrl;
+    String pokemonTypeUrl = "/pokemon-types/";
 
     public List<PokemonType> listPokemonsTypes() {
-        return null;
+        return List.of(restTemplate.getForObject(pokemonServiceUrl+pokemonTypeUrl,PokemonType[].class));
     }
 
     @Autowired
     void setRestTemplate(RestTemplate restTemplate) {
-
+        this.restTemplate = restTemplate;
     }
 
+    @Value("${pokemonType.service.url}")
     void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
-
+        this.pokemonServiceUrl = pokemonServiceUrl;
     }
 }

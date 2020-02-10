44003a6d8b73a0cd42e49ccd5dfa7f5a034cc570
Timothey-V <Tim170298>
2020-02-10 13:06:11 +0100

    Add RestConfiguration for change Local Language

--- src/main/java/com/miage/altea/game_ui/config/RestConfiguration.java
+++ src/main/java/com/miage/altea/game_ui/config/RestConfiguration.java
@@ -1,8 +1,14 @@
+
 package com.miage.altea.game_ui.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Configuration
 public class RestConfiguration {
@@ -10,6 +16,14 @@ public class RestConfiguration {
     @Bean
     RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Autowired
+    public void configureLocalInterceptor (RestTemplate rt){
+        rt.setInterceptors(List.of((httpRequest, bytes, clientHttpRequestExecution) -> {
+            httpRequest.getHeaders().add(HttpHeaders.ACCEPT_LANGUAGE, LocaleContextHolder.getLocale().toLanguageTag());
+            return clientHttpRequestExecution.execute(httpRequest,bytes);
+        }));
     }
 
 }
